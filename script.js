32c877032039f38347937083c9054d7941829a90
gemo <kim.gemo@gmail.com>
2024-09-01 16:57:41 +0200

    finishing selectable category section

--- script.js
+++ script.js
@@ -4,14 +4,15 @@ async function loadFilms(url) {
     return data;
 };
 
-
+// BEST FILM
 async function updateBestFilm() {
-    endPoint = "http://localhost:8000/api/v1/titles/?imdb_score_min=9&sort_by=-imdb_score";
+    endPoint = "http://localhost:8000/api/v1/titles/?lang_contains=fr&sort_by=-imdb_score";
     let bestFiveFilms = await loadFilms(endPoint);
     bestFilm = bestFiveFilms.results[0];
 
     // BEST FILM
     document.getElementById("bestFilmTitle").innerText = bestFilm.title;
+    document.getElementById("bestFilmTitle").parentElement.id = bestFilm.id;
     document.getElementById("bestFilmPicture").src = bestFilm.image_url;
     document.getElementById("bestFilmPicture").alt = bestFilm.title;
     bestFilmMoreData = await loadFilms(bestFilm.url);
@@ -19,7 +20,10 @@ async function updateBestFilm() {
 
 }
 
+updateBestFilm()
+// --------------------------------------------------
 
+// BEST FILMS PER CATEGORY
 async function updateCategoryFilms(url, id) {
     let fiveFilms = await loadFilms(url);
 
@@ -34,63 +38,70 @@ async function updateCategoryFilms(url, id) {
                             <div class="position-absolute top-50 start-0 w-100 bg-dark d-flex flex-column p-2"
                                 style="--bs-bg-opacity: 0.6;">
                                 <strong class="text-light align-self-center text-center" style="font-size: x-large">${film.title}</strong>
-                                <button type="button" class="btn btn-sm btn-dark align-self-end">Détails</button>
+                                <button type="button" id="detailsBtn" class="btn btn-sm btn-dark align-self-end">Détails</button>
                             </div>
                         </div>`
         row.appendChild(div);
     }
 
     // SIXTH FILM
-    const followingFiveFilms = await loadFilms(fiveFilms.next);
-    const sixthFilm = followingFiveFilms.results[0];
+    if (fiveFilms.next !== null) {
+        const followingFiveFilms = await loadFilms(fiveFilms.next);
+        const sixthFilm = followingFiveFilms.results[0];
 
-    const div = document.createElement('div');
-        div.className = 'col-sm-12 col-md-6 col-lg-4 mb-4';
-        div.innerHTML = `<div class="position-relative overflow-hidden" style="height: 350px;">
-                <img class="w-100 h-100 object-fit-cover" src=${sixthFilm.image_url} alt=${sixthFilm.title}>
-                <div class="position-absolute top-50 start-0 w-100 bg-dark d-flex flex-column p-2"
-                    style="--bs-bg-opacity: 0.7;">
-                    <strong class="text-light align-self-center text-center" style="font-size: x-large">${sixthFilm.title}</strong>
-                    <button type="button" class="btn btn-sm btn-dark align-self-end">Détails</button>
-                </div>
-            </div>
-        </div>`
-    row.appendChild(div);
+        const div = document.createElement('div');
+            div.className = 'col-sm-12 col-md-6 col-lg-4 mb-4';
+            div.innerHTML = `<div class="position-relative overflow-hidden" style="height: 350px;">
+                                <img class="w-100 h-100 object-fit-cover" src=${sixthFilm.image_url} alt=${sixthFilm.title}>
+                                <div class="position-absolute top-50 start-0 w-100 bg-dark d-flex flex-column p-2"
+                                    style="--bs-bg-opacity: 0.7;">
+                                    <strong class="text-light align-self-center text-center" style="font-size: x-large">${sixthFilm.title}</strong>
+                                    <button type="button" id="detailsBtn" class="btn btn-sm btn-dark align-self-end">Détails</button>
+                                </div>
+                            </div>`
+        row.appendChild(div);
+    }
+    
 };
 
-
-// ------------------------------------------ //
-updateBestFilm()
-const bestFilmsUrl = "http://localhost:8000/api/v1/titles/?imdb_score_min=9&sort_by=-imdb_score";
+const bestFilmsUrl = "http://localhost:8000/api/v1/titles/?lang_contains=fr&sort_by=-imdb_score";
 updateCategoryFilms(bestFilmsUrl, 'bestMoviesRow');
 
-const categoryUrl = "http://localhost:8000/api/v1/titles/?genre=mystery&sort_by=-imdb_score";
+const categoryUrl = "http://localhost:8000/api/v1/titles/?genre=crime&lang_contains=fr&sort_by=-imdb_score";
 updateCategoryFilms(categoryUrl, 'firstCategoryRow')
 
-const secondCategoryUrl = "http://localhost:8000/api/v1/titles/?genre=comedy&sort_by=-imdb_score";
+const secondCategoryUrl = "http://localhost:8000/api/v1/titles/?genre=comedy&lang_contains=fr&sort_by=-imdb_score";
 updateCategoryFilms(secondCategoryUrl, 'secondCategoryRow')
+// --------------------------------------------------
+
+// DROPDOWN MENU
+function dropdownBtn() {
+    document.getElementById("dropdown-menu").classList.toggle("show");
+}
+
+window.onload = function dropdownClicked() {
+    const categories = document.getElementsByClassName("dropdown-item")
+    for (category of categories){
+        category.addEventListener("click", function() {
+            document.getElementById("category-button").innerHTML = this.innerHTML;
+            const selectableCategoryRow = `http://localhost:8000/api/v1/titles/?genre=${this.innerHTML}&lang_contains=fr&sort_by=-imdb_score`
+            updateCategoryFilms(selectableCategoryRow, 'selectableCategoryRow')
+        })
+    }
+    document.getElementById("dropdown-default").click()
+}
+// --------------------------------------------------
 
 
-// const bestFilmsUrl = "http://localhost:8000/api/v1/titles/?imdb_score_min=9&sort_by=-imdb_score"
-// updateCategoryFilms(bestFilmsUrl, 'bestMoviesRow')
-// .then((_) => {
-//     const categoryUrl = "http://localhost:8000/api/v1/titles/?genre=mystery&sort_by=-imdb_score";
-//     return updateCategoryFilms(categoryUrl, 'firstCategoryRow')
-// })
-// .then((_) => {
-//     const secondCategoryUrl = "http://localhost:8000/api/v1/titles/?genre=comedy&sort_by=-imdb_score";
-//     updateCategoryFilms(secondCategoryUrl, 'secondCategoryRow')
-// })
 
-
-
+// MODAL WINDOWS
 document.addEventListener("DOMContentLoaded", function() 
 {
-	const openModalBtn = document.getElementById("bestMovieBtn");
+	const openModalBtn = document.getElementById("detailsBtn");
     const modal = document.getElementById("modal");
     const closeModalBtn = document.getElementById("close-modal");
 
-    openModalBtn.onclick = function() {
+    openModalBtn.onclick = async function() {
         modal.style.display = "block";
     }
 
@@ -98,4 +109,5 @@ document.addEventListener("DOMContentLoaded", function()
         modal.style.display = "none";
     } 
 })
+// --------------------------------------------------
 
